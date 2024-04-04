3649b740336bb5f13c860fab501aa1dba34781a9
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-04-04 23:34:58 +0500

    dds

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let button = document.querySelector("#btn");
+button.addEventListener("click", function () {
+  function RandomColor() {
+    let randomNum1 = Math.floor(Math.random() * 255);
+    let randomNum2 = Math.floor(Math.random() * 200);
+    let randomNum3 = Math.floor(Math.random() * 150);
+    return `rgb(${randomNum1},${randomNum2},${randomNum3})`;
+  }
+  container.style.backgroundImage = `linear-gradient(217deg, ${RandomColor()}, ${RandomColor()} 70.71%)`;
+
+  btn.style.backgroundImage = ` linear-gradient(127deg, ${RandomColor()}, ${RandomColor()} 70.71%)`;
+
+  text.style.color = RandomColor();
+  btn.style.borderColor = `linear-gradient(${RandomColor()},${RandomColor()})`;
+});
